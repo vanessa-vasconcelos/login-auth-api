1c369f2a3ba4c348fbc6a7d33f9ca089b581ff21
Vanessa Vasconcelos <vanessa.vasconcelos434@gmail.com>
2025-05-06 23:14:53 -0300

    Conserto de cors

--- src/main/java/com/example/login_auth_api/infra/cors/CorsConfiguration.java
+++ src/main/java/com/example/login_auth_api/infra/cors/CorsConfiguration.java
@@ -9,7 +9,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
+        registry.addMapping("**")
             .allowedOrigins("http://localhost:4200")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
